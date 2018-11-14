33501712a0ff86cff6331a2a82ae7394fa45c9d1
Christina Davies <christinahdavies@gmail.com>
2018-11-14 14:50:23 +0000

    created README file

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' ) if development?
 require_relative('controllers/merchants_controller')
-require_relative('controllers/tags_controller')
 require_relative('controllers/wallet_controller')
 require_relative('controllers/transactions_controller')
 also_reload('models/*')
