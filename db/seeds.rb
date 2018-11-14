75d1c4f8799f5c36c00d975e252d5c46bb620aea
Christina Davies <christinahdavies@gmail.com>
2018-11-14 20:55:40 +0000

    added option to add new merchant on edit transaction view

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ merchant3 = Merchant.new({'merchant_name' => 'Waterstones'})
 merchant3.save
 merchant4 = Merchant.new({'merchant_name' => 'Odeon Cinemas'})
 merchant4.save
-merchant5 = Merchant.new({'merchant_name' => "Laings"})
+merchant5 = Merchant.new({'merchant_name' => "Tiffanys"})
 merchant5.save
 merchant6 = Merchant.new({'merchant_name' => "Harrods"})
 merchant6.save
@@ -26,6 +26,8 @@ merchant7 = Merchant.new({'merchant_name' => "The Ritz"})
 merchant7.save
 merchant8 = Merchant.new({'merchant_name' => "British Airways"})
 merchant8.save
+merchant9 = Merchant.new({'merchant_name' => "Misc"})
+merchant9.save
 
 
 tag1 = Tag.new({'tag_name' => 'Clothes'})
@@ -36,6 +38,7 @@ tag5 = Tag.new({'tag_name' => 'Dining out'})
 tag6 = Tag.new({'tag_name' => 'Subscription'})
 tag7 = Tag.new({'tag_name' => 'Personal care'})
 tag8 = Tag.new({'tag_name' => 'Travel'})
+tag9 = Tag.new({'tag_name' => 'Misc'})
 tag1.save
 tag2.save
 tag3.save
@@ -44,10 +47,11 @@ tag5.save
 tag6.save
 tag7.save
 tag8.save
+tag9.save
 
 wallet1 = Wallet.new({
   'wallet_name' => "Your budget",
-  'budget_amount' => 200.00,
+  'budget_amount' => 500.00,
   'budget_start_date' => '2018-11-01',
   'budget_end_date' => '2018-11-30'
   })
